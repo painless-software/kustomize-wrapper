1eb109ecc2aef2bece5edcff981598b412f9dc05
Peter Bittner <peter@painless.software>
2020-05-15 17:55:46 +0200

    Extract only the Go binary from the archives

---
+++ kustomize/helpers/archive.py
@@ -0,0 +1,59 @@
+"""
+Enhanced version of ``unpack_archive`` in a helper module
+
+shutil.unpack_archive() handles various archive formats (e.g. .zip, .tgz),
+but it only extracts the entire content of those files. We only want a
+single file from the archive, but don't want to go the extra route using
+Python's ``tarfile`` and ``zipfile`` modules, separately, in user code.
+"""
+import pathlib
+import tarfile
+import zipfile
+
+
+def unpack_archive(filename, extract_dir, *files):
+    """Unpack files from a Tar or Zip archive"""
+
+    archive_method = {
+        '.tar.gz': unpack_tar_archive,
+        '.tgz': unpack_tar_archive,
+        '.zip': unpack_zip_archive,
+    }
+    filetype = get_file_extension(filename)
+    try:
+        archive_method[filetype](filename, extract_dir, *files)
+    except KeyError:
+        raise NotImplementedError(f"Archive not supported: {filetype}")
+
+
+def unpack_tar_archive(filename, extract_dir, *files):
+    """Unpack files from a Tar archive"""
+    archive = tarfile.open(filename)
+    for member in files:
+        archive.extract(member, extract_dir)
+    archive.close()
+
+
+def unpack_zip_archive(filename, extract_dir, *files):
+    """Unpack files from a Zip archive"""
+    archive = zipfile.ZipFile(filename)
+    for member in files:
+        archive.extract(member, extract_dir)
+    archive.close()
+
+
+def get_file_extension(filename):
+    """
+    Reliably determine relevant file extension of archive
+
+    >>> get_file_extension("/tmp/foo_v3.5.5_linux_amd64.tgz")
+    '.tgz'
+    >>> get_file_extension("/tmp/foo_v1.2.3_darwin.1.212.tar.gz")
+    '.tar.gz'
+    >>> get_file_extension("/tmp/2020.12.31_windows.1.007.zip")
+    '.zip'
+    """
+    ext_list = pathlib.Path(filename).suffixes
+    good_exts = [_ for _ in ext_list[-2:]
+                 if 3 <= len(_) <= 4 and _[1:].isalpha()]
+    return ''.join(good_exts)
---
+++ tests/test_helpers_archive.py
@@ -0,0 +1,60 @@
+"""
+Tests for the archive helper module
+"""
+import pytest
+
+from unittest.mock import call, patch
+
+import kustomize
+
+
+@patch('kustomize.helpers.archive.unpack_zip_archive')
+@patch('kustomize.helpers.archive.unpack_tar_archive')
+def test_unpack_archive(mock_unpack_tar, mock_unpack_zip):
+    """
+    Are all supported archive types handled properly?
+    """
+    kustomize.helpers.archive.unpack_archive('/tmp/foo.tar.gz', '/tmp', 'bar')
+    assert mock_unpack_tar.mock_calls == [
+        call('/tmp/foo.tar.gz', '/tmp', 'bar')
+    ]
+
+    kustomize.helpers.archive.unpack_archive('/tmp/foo.zip', '/tmp', 'myfile')
+    assert mock_unpack_zip.mock_calls == [
+        call('/tmp/foo.zip', '/tmp', 'myfile')
+    ]
+
+    with pytest.raises(NotImplementedError):
+        kustomize.helpers.archive.unpack_archive('/tmp/foo.xyz', '/tmp', 'baz')
+
+
+@patch('tarfile.open')
+def test_untar_files(mock_tarfile):
+    """
+    Are we looping through the file list correctly?
+    """
+    kustomize.helpers.archive.unpack_tar_archive(
+        'foobar.tar.gz', '/tmp', 'hello.sh', 'LICENSE')
+
+    assert mock_tarfile.mock_calls == [
+        call('foobar.tar.gz'),
+        call().extract('hello.sh', '/tmp'),
+        call().extract('LICENSE', '/tmp'),
+        call().close(),
+    ]
+
+
+@patch('zipfile.ZipFile')
+def test_unzip_files(mock_zipfile):
+    """
+    Are we looping through the file list correctly?
+    """
+    kustomize.helpers.archive.unpack_zip_archive(
+        'foobar.zip', '/tmp', 'afile.exe', 'README.md')
+
+    assert mock_zipfile.mock_calls == [
+        call('foobar.zip'),
+        call().extract('afile.exe', '/tmp'),
+        call().extract('README.md', '/tmp'),
+        call().close(),
+    ]
