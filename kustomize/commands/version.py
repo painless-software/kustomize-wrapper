b14370e9bf4657b498640179f410ad6fe38d466c
Peter Bittner <peter@painless.software>
2020-05-15 12:46:46 +0200

    Add --update option to version command

--- kustomize/commands/version.py
+++ kustomize/commands/version.py
@@ -1,17 +1,24 @@
 """
 Supply version information of all components
 """
+import sys
+
 from .. import __version__
 from ..binaries import binarypath, run_piped_commands
-from ..download import ensure_binary
+from ..download import binary_exists, ensure_binary
 
 
-def version():
+def version(update=False):
     """Show version of all shipped components"""
     print(f"kustomize-wrapper {__version__}")
 
-    ensure_binary('kustomize')
-    ensure_binary('kubeval')
+    if update:
+        download_binaries()
+
+    if not binary_exists('kustomize'):
+        print(f"Go binary {binarypath('kustomize')} not available.")
+    if not binary_exists('kubeval'):
+        print(f"Go binary {binarypath('kubeval')} not available.")
 
     kustomize_result = run_piped_commands(
         [f"{binarypath('kustomize')} version"])
@@ -22,3 +29,27 @@ def version():
         print(f"Kustomize {kustomize_result.stdout.strip()}")
     if kubeval_result.stdout:
         print(f"Kubeval {kubeval_result.stdout.strip()}")
+
+
+def download_binaries():
+    """Ensure latest Go binaries are installed"""
+    kustomize_binary = binarypath('kustomize')
+    kubeval_binary = binarypath('kubeval')
+
+    try:
+        kustomize_binary.unlink()
+        print(f"Go binary {kustomize_binary} removed.")
+    except FileNotFoundError:
+        pass  # can be replaced by ``unlink(missing_ok=True)`` in Python 3.8+
+    except OSError as err:
+        print(f"Deleting {kustomize_binary} failed. {err}", file=sys.stderr)
+    ensure_binary('kustomize')
+
+    try:
+        kubeval_binary.unlink()
+        print(f"Go binary {kubeval_binary} removed.")
+    except FileNotFoundError:
+        pass  # can be replaced by ``unlink(missing_ok=True)`` in Python 3.8+
+    except OSError as err:
+        print(f"Deleting {kubeval_binary} failed. {err}", file=sys.stderr)
+    ensure_binary('kubeval')
--- tests/test_version.py
+++ tests/test_version.py
@@ -20,14 +20,63 @@ def test_cli_command(mock_command):
     assert mock_command.called
 
 
+@patch('kustomize.commands.version.download_binaries')
+def test_cli_update_option(mock_downloadbinaries):
+    """
+    Is the correct code called when invoked via the CLI?
+    """
+    with ArgvContext('kustomize', 'version', '--update'), \
+            pytest.raises(SystemExit):
+        kustomize.cli.main()
+
+    assert mock_downloadbinaries.called
+
+
+@patch('builtins.print')
+@patch('kustomize.commands.version.binary_exists', return_value=False)
+def test_explain_when_missing(mock_binaryexists, mock_print):
+    """
+    Do we explain that binaries are not available?
+    """
+    kustomize_binary = kustomize.commands.version.binarypath('kustomize')
+    kubeval_binary = kustomize.commands.version.binarypath('kubeval')
+
+    kustomize.commands.version.version()
+
+    assert mock_print.mock_calls == [
+        call(f"kustomize-wrapper {kustomize.__version__}"),
+        call(f"Go binary {kustomize_binary} not available."),
+        call(f"Go binary {kubeval_binary} not available."),
+    ]
+
+
+@patch('pathlib.Path.unlink')
 @patch('kustomize.commands.version.ensure_binary')
-def test_ensure_binaries(mock_ensurebinary):
+def test_ensure_binaries(mock_ensurebinary, mock_unlink):
     """
     Do we ensure binaries are available?
     """
-    kustomize.commands.version.version()
+    kustomize.commands.version.version(update=True)
 
     assert mock_ensurebinary.mock_calls == [
         call('kustomize'),
         call('kubeval'),
     ], "We don't ensure all binaries are available"
+
+
+@patch('builtins.print')
+@patch('kustomize.commands.version.ensure_binary')
+@patch('pathlib.Path.unlink', side_effect=OSError)
+def test_report_deletefailed(mock_unlink, mock_ensurebinary, mock_print):
+    """
+    Do we handle errors gracefully?
+    """
+    kustomize_binary = kustomize.commands.version.binarypath('kustomize')
+    kubeval_binary = kustomize.commands.version.binarypath('kubeval')
+
+    kustomize.commands.version.download_binaries()
+    kust_args, _ = mock_print.call_args_list[0]
+    kube_args, _ = mock_print.call_args_list[1]
+
+    assert kust_args[0].split(' failed. ')[0] == f"Deleting {kustomize_binary}"
+    assert kube_args[0].split(' failed. ')[0] == f"Deleting {kubeval_binary}"
